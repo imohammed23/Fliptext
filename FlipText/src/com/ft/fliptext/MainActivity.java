bf24eca9f7a8aaaccc8836ca337c522dbc9dda30
Imran <imohammed23@gmail.com>
2012-09-17 05:56:56 +0530

    Data access using Threads and XML file(unicode.xml).
    Added Share.
    Optimized application.

--- FlipText/src/com/ft/fliptext/MainActivity.java
+++ FlipText/src/com/ft/fliptext/MainActivity.java
@@ -1,14 +1,17 @@
 package com.ft.fliptext;
 
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Hashtable;
 
-import android.*;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
@@ -20,11 +23,7 @@ import android.widget.TextView;
  * @author Imran
  *
  */
- 	
-/**
- * @author Imran
- *
- */
+
 public class MainActivity extends Activity {
 	
 	private TextView inputText, flipText;
@@ -34,55 +33,77 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-      //Load the text file into hash table
-    	final Hashtable<String, String> characters = flipCharHashTable();
+        final Resources res = getResources();
+        //Populate data into hastable using threads and XML file
+    	final Hashtable<String, String> charactersTest = new LoadDataClass().doInBackground(res);
+    	
         
         inputText = (TextView)findViewById(R.id.inputText);
         inputText.addTextChangedListener(new TextWatcher(){
-
-			@Override
-			public void afterTextChanged(Editable arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void beforeTextChanged(CharSequence arg0, int arg1,
-					int arg2, int arg3) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			/***
+		/***
 			 * On TextChange - User entering a key an action to reverse the Char sequence and 
 			 * input in another text box is performed.
 			 */
 			@Override
 			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
 					int arg3) {
-				// TODO Auto-generated method stub
 				
- 
 		        flipText = (TextView) findViewById(R.id.flipText);
 		        CharSequence charS = inputText.getText();
-		        flipText.setText(flipString(charS, characters));
+		        flipText.setText(flipString(charS, charactersTest));
 				
+			}
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+			
+			}
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+				// TODO Auto-generated method stub
+			
 			}
         });
         
-        final Button button = (Button) findViewById(R.id.clear);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button clear = (Button) findViewById(R.id.clear);
+        clear.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
             	inputText.setText("");
             }
         });
+        
+        final Button share = (Button) findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+			
+        	
+			@Override
+			public void onClick(View v) {
+				if(flipText.getText()!= null || flipText.getText()!= "")
+					OnClickShare();
+			}
+		});
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    /**
+     * Method to share content via other apps
+     * @param view
+     */
+    public void OnClickShare()
+    {
+    	Intent shareIntent = new Intent(Intent.ACTION_SEND);
+    	shareIntent.setType("text/plain");
+    	shareIntent.putExtra(Intent.EXTRA_SUBJECT,"Flipped Text");
+    	shareIntent.putExtra(Intent.EXTRA_TEXT, flipText.getText().toString());
+    	startActivity(Intent.createChooser(shareIntent, "Share via"));
     }
     
 
@@ -99,75 +120,85 @@ public class MainActivity extends Activity {
     		if(characters.containsKey(Character.toLowerCase(charS.charAt(i))+""))
     		{
     			fText += characters.get(Character.toLowerCase(charS.charAt(i))+"");
-    		}else
-    		{
-    			fText += Character.toLowerCase(charS.charAt(i));
+    			continue;
     		}
+    			fText += Character.toLowerCase(charS.charAt(i));
     	}
     	return fText;
     }
     
-    /***
-     * Method to read text data and add keys and values of letters to hash table.
-     * @return
-     */
-    public Hashtable<String, String> flipCharHashTable()
-    {
-    	Hashtable<String, String> characters = new Hashtable<String, String>();
-    	
-    	try{
-    		
-    		  //Read input file from assets-unicode.txt
-    		  InputStream in = getAssets().open("unicode.txt");
-			  BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-16"));
-			  String strLine;
-			  //Read each line and adding chars key and value to hash table
-			  while ((strLine = br.readLine()) != null)   {
-			  
-				  String letters[] = strLine.split("#");
-				  
-				  if(letters[0].startsWith("\\u"))
-				  {
-					  String lett = ConvertByteCodetoString(letters[0]);
-					  if(letters[1].startsWith("\\u"))
-					  {
-						  String lett1 = ConvertByteCodetoString(letters[1]);
-						  characters.put(lett, lett1);
-					  }
-					  else
-					  {
-						  characters.put(lett, letters[1]);
-					  }
-				  }else
-				  {
-					  if(letters[1].startsWith("\\u"))
-					  {
-						  String lett1 = ConvertByteCodetoString(letters[1]);
-						  characters.put(letters[0], lett1);
-					  }
-					  else
-					  {
-						  characters.put(letters[0], letters[1]);
-					  }
-				  }
-			  }
-			  //Close the input stream
-			  in.close();
-			    }catch (Exception e){//Catch exception if any
-			  System.err.println("Error: " + e.getMessage());
-			  }
-    	return characters;
+}
 
-    }
-    
-    /***
-     * Convert Byte code from file to String
-     * @param bString
-     * @return
-     */
-    
-    public String ConvertByteCodetoString(String bString)
+
+/***
+ * 
+ * @author Imran
+ *
+ *
+ *This class create a working thread to populate data into the hash table.
+ *This thread works completely unrelated to the UI thread.
+ *Although this is not necessary for a small program like this.
+ *It is a good practice to Acess and load data in the background.
+ *
+ *Now accesses data from a XML file.(Earlier a text file)
+ */
+class LoadDataClass extends AsyncTask<Resources, Void, Hashtable<String, String>>{
+
+	/**
+	 * Method runs in the background and loads data into the hash table.
+	 */
+	@Override
+	protected Hashtable<String, String> doInBackground(Resources... params) {
+		Hashtable<String, String> characters = new Hashtable<String, String>();
+		XmlPullParser xmlR =  params[0].getXml(R.xml.unicode);
+		int eventType;
+		try {
+			
+			eventType = xmlR.next();
+			while(eventType != XmlPullParser.END_DOCUMENT)
+			{
+				String key = null;
+				String value = null;
+				if(eventType == XmlPullParser.START_TAG)
+				{
+					if(xmlR.getName().equals("string"))
+					{
+					key = ConvertByteCodetoString(xmlR.getAttributeValue(0));
+					value = ConvertByteCodetoString(xmlR.nextText());
+					}
+					else
+					{
+						eventType = xmlR.next();
+						continue;
+					}
+				}
+				
+				if(key != null || value != null)
+				{
+					characters.put(key, value);
+				}
+				
+				eventType = xmlR.next();
+				
+			}
+		} catch (XmlPullParserException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return characters;
+	}
+	
+	/**
+	 * Method to convert byte code into string.
+	 * @param bString
+	 * @return
+	 */
+	public String ConvertByteCodetoString(String bString)
     {
+		if(bString.startsWith("\\u"))
+		{
     	String lett = "";
     	String temp = bString.split(" ")[0];
 		  temp = temp.replace("\\","");
@@ -177,6 +208,9 @@ public class MainActivity extends Activity {
 			  lett += (char)hexVal;
 			  }
     	return lett;
+		}
+		return bString;
     }
-    
+	
 }
+	
